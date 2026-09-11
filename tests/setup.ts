df9395aa39a97b3898935dae982464165a581574
Adriano Aimar <adriano.aimar12@gmail.com>
2026-09-11 17:14:32 -0500

    test: silence jsdom navigation warning

--- tests/setup.ts
+++ tests/setup.ts
@@ -4,4 +4,5 @@ import { beforeEach } from 'vitest';
 beforeEach(() => {
   window.localStorage.clear();
   window.sessionStorage.clear();
+  Object.defineProperty(window, 'scrollTo', { configurable: true, value: () => undefined });
 });
